a7f33c6f71d40c140e9fa1e5cd514e4398adaaff
Leandro de Jesus <lean_silva100@hotmail.com>
2023-01-20 10:50:41 -0300

    1 commit, feito o html dos seguintes componentes: barra de navegação e menu principal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PagPrincipalComponent } from './components/pag-principal/pag-principal.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: PagPrincipalComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
