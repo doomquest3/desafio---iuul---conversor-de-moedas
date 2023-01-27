d642106e82ff8059aa9d5e625428c352bc5ebe22
Leandro de Jesus <lean_silva100@hotmail.com>
2023-01-27 15:11:06 -0300

    5º commit, adicionando tela de listagem de moedas, que será utilzado para mostrar dados requisitados na pela utilizando a API disponibilizada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListaMoedasComponent } from './components/lista-moedas/lista-moedas.component';
 import { PagPrincipalComponent } from './components/pag-principal/pag-principal.component';
 
 const routes: Routes = [
-  {path:'', component: PagPrincipalComponent}
+  {path:'', component: PagPrincipalComponent},
+  {path:'list', component: ListaMoedasComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,23 +4,36 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+//Componentes
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { PagPrincipalComponent } from './components/pag-principal/pag-principal.component';
-import { MatIconModule } from '@angular/material/icon';
+import { ListaMoedasComponent } from './components/lista-moedas/lista-moedas.component';
 
+//Angular Material
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatInputModule } from '@angular/material/input';
+import { MatPaginatorModule } from '@angular/material/paginator';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
     PagPrincipalComponent,
+    ListaMoedasComponent,
 
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatIconModule
+    MatIconModule,
+    MatListModule,
+    MatMenuModule,
+    MatInputModule,
+    MatPaginatorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
